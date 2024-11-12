2a4d1a42f6ee3f5df6097b4839893b200fdae60f
iamnonothing <allo290216@gmail.com>
2024-11-12 09:02:00 +0200

    v2.1

--- main.py
+++ main.py
@@ -5,20 +5,19 @@ from openai import OpenAI
 import pickle
 import threading
 import time
-import re
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 modes = {}
-
+ignore = {}
+admins_list = {'stratofortress_b52'}
 
 try:
     with open("data.pkl", "rb") as f:
-        modes, modes_fmt = pickle.load(f)
+        admins_list, modes = pickle.load(f)
 except Exception as e:
     print(e)
-
 
 with open("openai.txt", "r") as f:
     client = OpenAI(api_key=f.read())
@@ -66,22 +65,23 @@ async def chat(ctx):
 
 
 @bot.command()
-async def mode(ctx):
-    match = re.match(r"!mode ([\w_{}]+) (on|off) (<@\d+>)", ctx.message.content)
-    if match:
-        fmt, _mode, _ = match.groups()
-        fmt = fmt.replace('_', ' ')
-        user = ctx.message.mentions[0].name
+async def mode(ctx, fmt: str, _mode: str, user: discord.Member):
+    if ctx.message.author.name not in admins_list:
+        await ctx.send("You're not an admin!")
+        return
 
-        if fmt not in modes:
-            modes[fmt] = set()
+    fmt = fmt.replace('_', ' ')
+    user = user.name
 
-        if _mode == "on":
-            modes[fmt].add(user)
-        elif _mode == "off":
-            modes[fmt].discard(user)
-    else:
-        await ctx.send("Usage: !mode <fmt> <on|off> <@user>")
+    if fmt not in modes:
+        modes[fmt] = set()
+
+    if _mode == "on":
+        modes[fmt].add(user)
+        await ctx.send(f"{fmt.replace('{}', user)}'ing {user}")
+    elif _mode == "off":
+        modes[fmt].discard(user)
+        await ctx.send(f"stopped {fmt.replace('{}', user)}'ing {user}")
 
 
 @bot.event
@@ -93,18 +93,48 @@ async def on_message(message):
         if message.author.name in users:
             await message.channel.send(fmt.replace('{}', message.author.mention))
 
-    await bot.process_commands(message)
+    if message.author.name not in ignore:
+        await bot.process_commands(message)
+
+
+@bot.command()
+async def admins(ctx):
+    if admins_list:
+        admin_list = ', '.join(admins_list)
+        await ctx.send(f"Current admins: {admin_list}")
+    else:
+        await ctx.send("No admins available.")
+
+
+@bot.command()
+async def admin(ctx, action: str, member: discord.Member):
+    if ctx.message.author.name not in admins_list:
+        await ctx.send("You're not an admin!")
+        return
+
+    if action not in ("add", "remove"):
+        await ctx.send("Usage: !admin <add/remove> <@user>")
+        return
+
+    if action == "add":
+        admins_list.add(member.name)
+        await ctx.send(f"{member.name} was added to the admins.")
+    elif action == "remove":
+        if member.name in admins_list:
+            admins_list.remove(member.name)
+            await ctx.send(f"{member.name} was removed from the admins.")
+        else:
+            await ctx.send(f"{member.name} is not an admin.")
 
 
 def save_modes():
     while True:
         time.sleep(60)
-        with open("data.pkl", "wb") as _f:
-            pickle.dump([modes, modes_fmt], _f)
+        with open("data.pkl", "wb+") as _f:
+            pickle.dump([admins_list, modes], _f)
 
 
 threading.Thread(target=save_modes).start()
-
 
 with open("discord.txt") as f:
     token = f.read()
